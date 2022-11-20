bcbbacea53bd7ce5ce6d04b0f35d2f26da719d1e
danielmed <daniel.medina@talent.com>
2022-11-20 18:07:52 -0500

    getday ny date change to post method

--- router/day.js
+++ router/day.js
@@ -4,7 +4,7 @@ const DayController = require("../controllers/day")
 
 const api = express.Router();
 
-api.get("/day/getday",DayController.getDay);
+api.post("/day/getday",DayController.getDay);
 api.post("/day/create", DayController.createDay);
 api.patch("/day/update", DayController.updateDay);
 api.post("/day/dayuserlist", DayController.userListByDay);
